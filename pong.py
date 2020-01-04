29fa87307634818752c392cf81f1cd9618d254ed
Anton <Hayartur1599@gmailcom>
2020-01-04 13:54:02 +0000

    Modyfied pong.py

--- pong.py
+++ pong.py
@@ -1,27 +1,18 @@
 import pygame, random
 from pygame.locals import *
 
-pygame.init()
-
-SIZE = WIDTH, HEIGHT = 640, 480
-
-_display_surf = pygame.display.set_mode(SIZE, pygame.HWSURFACE | pygame.DOUBLEBUF)
-
-FONT = pygame.font.Font(None, 36)
-
-
 class App:
 
     class Ball:
-        def __init__(self):
-            self._center = [int(WIDTH / 2) - 10, int(HEIGHT / 2) - 10]
+        def __init__(self, width, height, surf):
+            self._center = [int(width / 2) - 10, int(height / 2) - 10]
             self.horDir = True
             self.vertDir = 1
             self._radius = 10
-            self._ball = pygame.draw.circle(_display_surf, (255, 255, 255), self._center, self._radius)
+            self._ball = pygame.draw.circle(surf, (255, 255, 255), self._center, self._radius)
 
-        def render(self):
-            self._ball = pygame.draw.circle(_display_surf, (255, 255, 255), self._ball.center, self._radius)
+        def render(self, surf):
+            self._ball = pygame.draw.circle(surf, (255, 255, 255), self._ball.center, self._radius)
 
         def move_right(self):
             self._ball = self._ball.move(3, 7 * self.vertDir)
@@ -32,8 +23,8 @@ class App:
         def top(self):
             return self._ball.top
 
-        def border_collision(self):
-            if self._ball.top <= 0 or self._ball.top > HEIGHT - 2*self._radius:
+        def border_collision(self, height):
+            if self._ball.top <= 0 or self._ball.top > height - 2*self._radius:
                 return True
             else:
                 return False
@@ -50,19 +41,22 @@ class App:
             else:
                 return False
 
-        def right_goal_collision(self):
-            if self._ball.x >= WIDTH - 15:
+        def right_goal_collision(self, width):
+            if self._ball.x >= width - 15:
                 return True
             else:
                 return False
 
-        def back_to_origin(self):
-            self._ball = pygame.draw.circle(_display_surf, (255, 255, 255), self._center, self._radius)
+        def back_to_origin(self, surf):
+            self._ball = pygame.draw.circle(surf, (255, 255, 255), self._center, self._radius)
             
     
     FPS = 30
 
     def __init__(self):
+        self.__size = self.__width, self.__height = 640, 480
+        self.__display_surf = None
+        self.__font = None
         self._leftScore, self._rightScore = 0, 0
         self._text_score = None
         self._text_place = None
@@ -73,15 +67,19 @@ class App:
         self._dirRight = 1
         self._dirLeft = 1
         self._clock = pygame.time.Clock()
-        self._leftPlatform = pygame.Rect([15, int(HEIGHT / 2) - 50, 15, 80])
-        self._rightPlatform = pygame.Rect([WIDTH - 30, int(HEIGHT / 2) - 50, 15, 80])
-        self._ball = App.Ball()
+        self._leftPlatform = pygame.Rect([15, int(self.__height / 2) - 50, 15, 80])
+        self._rightPlatform = pygame.Rect([self.__width - 30, int(self.__height / 2) - 50, 15, 80])
+        self._ball = None
         
 
     def on_init(self):
+        pygame.init()
+        self.__display_surf = pygame.display.set_mode(self.__size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.__font = pygame.font.Font(None, 36)
         pygame.display.set_caption("Pong")
-        self._text_score = FONT.render(str(self._leftScore) + " : " + str(self._rightScore), 0, (255, 255, 255))
-        self._text_place = self._text_score.get_rect(center=(305, 15))
+        self._text_score = self.__font.render(str(self._leftScore) + " : " + str(self._rightScore), 0, (255, 255, 255))
+        self._text_place = self._text_score.get_rect(center=(self.__width / 2 - 15, 15))
+        self._ball = App.Ball(self.width, self.height, self.__display_surf)
         self._running = True
 
     def on_event(self, event):
@@ -124,21 +122,20 @@ class App:
         if self._ball.left_goal_collision():
             self._play = False
             self._leftScore += 1
-            self._ball.back_to_origin()
+            self._ball.back_to_origin(self.__display_surf)
             self.update_score()
-        if self._ball.right_goal_collision():
+        if self._ball.right_goal_collision(self.__width):
             self._play = False
             self._rightScore += 1
-            self._ball.back_to_origin()
+            self._ball.back_to_origin(self.__display_surf)
             self.update_score()
 
     def update_score(self):
-        self._text_score = FONT.render(str(self._leftScore) + " : " + str(self._rightScore), 0, (255, 255, 255))
+        self._text_score = self.__font.render(str(self._leftScore) + " : " + str(self._rightScore), 0, (255, 255, 255))
 
     def start(self):
         self._play = True
         num = random.randint(1, 100)
-        print(num)
         if num > 50:
             self._ball.horDir = 1
             self._ball.move_left()
@@ -148,17 +145,17 @@ class App:
 
 
     def on_loop(self):
-        if self._move_leftPlatform and ((self._leftPlatform.top > 0 and self._dirLeft == -1) or (self._leftPlatform.top < HEIGHT - 80 and self._dirLeft == 1)):
+        if self._move_leftPlatform and ((self._leftPlatform.top > 0 and self._dirLeft == -1) or (self._leftPlatform.top < self.height - 80 and self._dirLeft == 1)):
             self.__move_leftPlatform(self._dirLeft)
-        if self._move_rightPlatform and ((self._rightPlatform.top > 0 and self._dirRight == -1) or (self._rightPlatform.top < HEIGHT - 80 and self._dirRight == 1)):
+        if self._move_rightPlatform and ((self._rightPlatform.top > 0 and self._dirRight == -1) or (self._rightPlatform.top < self.height - 80 and self._dirRight == 1)):
             self.__move_rightPlatform(self._dirRight)
         if self._play:
             if self._ball.horDir:
-                if self._ball.border_collision():
+                if self._ball.border_collision(self.__height):
                     self._ball.vertDir = - self._ball.vertDir
                 self._ball.move_left()
             if not self._ball.horDir:
-                if self._ball.border_collision():
+                if self._ball.border_collision(self.__height):
                     self._ball.vertDir = - self._ball.vertDir
                 self._ball.move_right()
             if self._ball.platform_collision(self._leftPlatform):
@@ -169,13 +166,13 @@ class App:
                 self._ball.move_left()
         self.goal_check()
         pygame.display.update()
-        _display_surf.fill((0, 0, 0))
+        self.__display_surf.fill((0, 0, 0))
 
     def on_render(self):
-        pygame.draw.rect(_display_surf, pygame.Color(255, 255, 255), self._leftPlatform)
-        pygame.draw.rect(_display_surf, pygame.Color(255, 255, 255), self._rightPlatform)
-        self._ball.render()
-        _display_surf.blit(self._text_score, self._text_place)
+        pygame.draw.rect(self.__display_surf, pygame.Color(255, 255, 255), self._leftPlatform)
+        pygame.draw.rect(self.__display_surf, pygame.Color(255, 255, 255), self._rightPlatform)
+        self._ball.render(self.__display_surf)
+        self.__display_surf.blit(self._text_score, self._text_place)
 
     def on_cleanup(self):
         pygame.quit()
@@ -191,6 +188,18 @@ class App:
             self.on_loop()
             self._clock.tick(self.FPS)
         self.on_cleanup()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def height(self):
+        return self.__height
+
+    @property
+    def width(self):
+        return self.__width
 
 
 if __name__ == "__main__":
